c8be69da5d1f634816428cc06322bbb6e7f16882
Alex Poling <alexp@corp.owwco.com>
2017-06-13 15:03:57 -0400

    post git push commit

--- Scouting/Scouting/Controllers/MatchController.cs
+++ Scouting/Scouting/Controllers/MatchController.cs
@@ -46,8 +46,6 @@ namespace Scouting.Controllers
         }
 
         // POST: Match/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "MatchID,TeamID,Score,Result,Shooter")] Match match)
